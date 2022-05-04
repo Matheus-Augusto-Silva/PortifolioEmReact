74df453069ce43d4bdd505c530fe16b7efa4c58d
Matheus-Augusto-Silva <matheusaugusto-18@hotmail.com>
2022-05-03 23:31:57 -0300

    adição projeto Dogs

--- src/components/portifolio/Portifolio.jsx
+++ src/components/portifolio/Portifolio.jsx
@@ -5,6 +5,9 @@ import { Item } from './Item';
 import imc from '../../images/imc.PNG'
 import coca from '../../images/coca.JPG'
 import ninja from '../../images/ninja.PNG'
+import dogz from '../../images/dogz.PNG'
+
+
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
   { width: 550, itemsToShow: 2 },
@@ -17,6 +20,9 @@ export const Portifolio = () => {
     <Container id='portifolio'>
       <h1>Portifólio</h1>
       <Carousel className='caroussel' breakPoints={breakPoints}>
+          <a href="https://dogz.netlify.app/" target="_blank">
+            <Item imgName={dogz} projectName={"Dogs-Consumindo API"}/>
+          </a>
           <a href="https://matheus-augusto-silva.github.io/Projeto-IMC/" target="_blank">
             <Item imgName={imc} projectName={"Calculador de IMC"}/>
           </a>
